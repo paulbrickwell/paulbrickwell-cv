cabe011a01c01143d0fd4d920732257476f9356a
paulbrickwell <paulbrickwell@gmail.com>
2021-08-02 14:26:06 -0700

    added bootcamps and adjusted skills

--- src/components/sidebar-components/Education.vue
+++ src/components/sidebar-components/Education.vue
@@ -2,6 +2,25 @@
   <div class="education">
     <h3 class="title">Education</h3>
     <div class="education-item">
+      <p>
+        <a
+          href="https://scrimba.com/certificate/uaVWv2sK/gjavascript"
+          target="_blank"
+          >JavaScript Bootcamp</a
+        ><img src="../../assets/icons/link.svg" />
+      </p>
+      <p>Scrimba</p>
+      <p>2021</p>
+    </div>
+    <div class="education-item">
+      <p>
+        <a href="https://scrimba.com/certificate/uaVWv2sK/gvue">Vue Bootcamp</a
+        ><img src="../../assets/icons/link.svg" />
+      </p>
+      <p>Scrimba</p>
+      <p>2021</p>
+    </div>
+    <div class="education-item">
       <p>BS, Graphic Design</p>
       <p>Full Sail University</p>
       <p>2014 - 2017</p>
--- src/components/sidebar-components/Skills.vue
+++ src/components/sidebar-components/Skills.vue
@@ -7,26 +7,24 @@
       <li>IT Strategy</li>
       <li>Network Design</li>
     </ul>
-    <ul class="skills-list">
-      <li>HTML / CSS<Rating5 /></li>
-      <li>JavaScript<Rating3 /></li>
-      <li>Vue.js<Rating2 /></li>
-      <li>React<Rating1 /></li>
+    <ul class="skills-list with-rating">
+      <li><Rating5 /> HTML / CSS</li>
+      <li><Rating4 /> JavaScript</li>
+      <li><Rating4 /> Vue.js</li>
+      <li><Rating1 /> React</li>
     </ul>
   </div>
 </template>
 
 <script>
 import Rating5 from "../utilities/Rating5.vue";
-import Rating3 from "../utilities/Rating3.vue";
-import Rating2 from "../utilities/Rating2.vue";
+import Rating4 from "../utilities/Rating4.vue";
 import Rating1 from "../utilities/Rating1.vue";
 
 export default {
   components: {
     Rating5,
-    Rating3,
-    Rating2,
+    Rating4,
     Rating1,
   },
 };
