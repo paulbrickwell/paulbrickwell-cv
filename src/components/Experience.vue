437cbe100d0d8a7c236eaf34f06416d6cf00e948
Paul Brickwell <paulbrickwell@gmail.com>
2021-10-24 11:53:13 -0700

    Updated experience and education

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="experience">
     <h3 class="title">Experience</h3>
-    <Freelance />
     <CraigTaylorEquipment />
     <Brickwell />
     <ImmediateCare />
@@ -9,14 +8,12 @@
 </template>
 
 <script>
-import Freelance from "./experience/Freelance.vue";
 import CraigTaylorEquipment from "./experience/CraigTaylorEquipment.vue";
 import Brickwell from "./experience/Brickwell.vue";
 import ImmediateCare from "./experience/ImmediateCare.vue";
 
 export default {
   components: {
-    Freelance,
     CraigTaylorEquipment,
     Brickwell,
     ImmediateCare,
