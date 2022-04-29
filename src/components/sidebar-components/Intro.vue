236563bd65e82419a0711aa1ed388b5718397cf1
Paul Brickwell <paulbrickwell@gmail.com>
2022-04-29 14:51:32 -0700

    Spelling fix

--- src/components/sidebar-components/Intro.vue
+++ src/components/sidebar-components/Intro.vue
@@ -2,11 +2,10 @@
   <div class="intro">
     <p>
       With over 13 years of experience managing IT systems, I have worked with
-      on on-site servers and entirely cloud-based solutions. I am currently
-      pursuing a career change into Front-End Web Development after spending the
-      last 5 years working on various web projects part time. I have completed
-      JavaScript and Vue.js bootcamps in addition to extensive and continues
-      self-training.
+      on-site servers and entirely cloud-based solutions. I am shifting focus to
+      Front-End Web Development after spending the last 5 years working on
+      various web projects part-time. I have completed JavaScript and Vue.js
+      bootcamps in addition to extensive and continues self-training.
     </p>
   </div>
 </template>
